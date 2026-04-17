33c1539088dac94e60e568a2dc21c994f872e70b
Raul9523 <raul@iMac-de-Raul.local>
2026-04-17 15:48:51 -0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Login from './login/Login.jsx';
+import Registro from './registro/Registro.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Login />
+    {/*<Registro />  */} 
+  </StrictMode>,
+)
---
+++ src/registro/Registro.jsx
@@ -0,0 +1,7 @@
+import Title from "../components/athoms/Title";
+function Registro({title}) {
+    return (<div className="bg-green-500">
+        <Title title="Registro" />
+    </div>);
+}
+export default Registro;
