2abdd5f81dbe179e2a457caca9a797fcd4ac921f
Raul9523 <raulpte0211@gmail.com>
2026-04-18 00:42:29 -0500

    security: ocultar configuración de Firebase en variables de entorno para evitar alertas de GitHub

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB7qTTBxIbF-RZfOnlHCJ1gh-vDP9MoZlM",
-  authDomain: "upbfullstack.firebaseapp.com",
-  projectId: "upbfullstack",
-  storageBucket: "upbfullstack.firebasestorage.app",
-  messagingSenderId: "554824273861",
-  appId: "1:554824273861:web:d29724b5b0dce9270605ac"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
